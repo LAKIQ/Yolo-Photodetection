af96fd9abf25bee45dce9b96bce8276f25b4a0b2
LAKIQ <104293375+LAKIQ@users.noreply.github.com>
2023-02-09 13:49:37 +0700

    Add About Data

---
+++ 4_webapp/pages/2_About.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+st.header("About Me")
+
+
+st.write("Hi my name is Sarit Thitikunvanit This is my prototype YOLO Training Project based on Udemy course for continue Development")
+
+st.subheader("Contact")
+
+st.write("Tel-Num : 096-242-2046")
+st.write("Gmail : Sarit.thitikunvanit@gmail.com")
+st.write("Git hub : https://github.com/LAKIQ")
+
+st.image("./images/Me.jpg", width=400)
